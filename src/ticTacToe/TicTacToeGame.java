2a2ddc28f26c0b0126394d78aafd40acb40d7544
chethanjs97 <chethanjschethu@gmail.com>
2020-11-05 14:38:02 +0530

    Added : Written method showBoard to display the current Board

--- src/ticTacToe/TicTacToeGame.java
+++ src/ticTacToe/TicTacToeGame.java
@@ -1,52 +1,40 @@
 package ticTacToe;
 import java.util.Scanner;
-
-
-
 public class TicTacToeGame {
-
 	static char[] board = new char[10];
+	static char player = ' ';
+	static char computer = ' ';
 	
-
 	public static void main(String[] args) {
-		char player = ' ';
-		char computer = ' ';
 		createBoard();
-		player = selectXorO();
-		if (player == 'X') {
-			computer = 'O';
-		} else {
-			computer = 'X';
-		}
-		System.out.println(player + " is player " + computer + " is computer");
+	    chooseXorO();
 		showBoard();
 	}
-	/* UC-3 Display Board */
+/* UC-3 Display Board */
 	private static void showBoard() {
-		for (int i = 1; i < board.length; i = i + 3) {
-			System.out.println("|" + board[i] + " |" + board[i + 1] + " |" + board[i + 2] + " |");
+		for (int index = 1; index < board.length; index = index + 3) {
+			System.out.println("|" + board[index] + " |" + board[index + 1] + " |" + board[index + 2] + " |");
 			System.out.println("-----------");
 		}
 	
 	}
 /* UC-2 Player Choose Letter X or O */
-	private static char selectXorO() {
-		char player = 'X';
+	private static void chooseXorO() {
 		Scanner scan = new Scanner(System.in);
 		while (true) {
 			System.out.println("Select either X or O");
-			player = scan.next().charAt(0);
-			if (player == 'x' || player == 'X') {
-				return 'X';
-			} else if (player == 'o' || player == 'O') {
-				return 'O';
+			player = scan.next().toUpperCase().charAt(0);
+			if (player == 'X' || player == 'O') {
+				computer = player == 'X' ? 'O' : 'X';
+				break;
 			} else {
 				System.out.println("Invalid choice,Enter again");
 				continue;
 			}
 		}
+		System.out.println(player + " is player " + computer + " is computer");
 	}
-	/* UC-1 Creating Board */
+/* UC-1 Creating Board */
 	static void createBoard() {
 		for (int index = 1; index < board.length; index++) {
 			board[index] = ' ';
