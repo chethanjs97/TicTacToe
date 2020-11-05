136f9116545b732dfcd03994a4ec1c2392b1f52d
chethanjs97 <chethanjschethu@gmail.com>
2020-11-05 11:35:31 +0530

    Added : Ability to allow the player to choose a letter x or O

--- src/ticTacToe/TicTacToeGame.java
+++ src/ticTacToe/TicTacToeGame.java
@@ -1,17 +1,46 @@
 package ticTacToe;
+import java.util.Scanner;
+
+
 
 public class TicTacToeGame {
 
+	static char[] board = new char[10];
+
 	public static void main(String[] args) {
-		char[] board = createBoard();
+		char player = ' ';
+		char computer = ' ';
+		createBoard();
+		player = selectXorO();
+		if (player == 'X') {
+			computer = 'O';
+		} else {
+			computer = 'X';
+		}
+		System.out.println(player + " is player " + computer + " is computer");
 	}
-	/* UC-1 */
-	private static char[] createBoard() {
-		char[] board = new char[10];
-		for(int index=1;index<board.length;index++){
+
+	private static char selectXorO() {
+		char player = 'X';
+		Scanner scan = new Scanner(System.in);
+		while (true) {
+			System.out.println("Select either X or O");
+			player = scan.next().charAt(0);
+			if (player == 'x' || player == 'X') {
+				return 'X';
+			} else if (player == 'o' || player == 'O') {
+				return 'O';
+			} else {
+				System.out.println("Invalid choice,Enter again");
+				continue;
+			}
+		}
+	}
+
+	static void createBoard() {
+		for (int index = 1; index < 10; index++) {
 			board[index] = ' ';
 		}
-		return board;
 	}
 
 }
